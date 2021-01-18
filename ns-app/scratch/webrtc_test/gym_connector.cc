c70fb43b3e4e9a007d6abecd25fe38a5194c7aa9
Ze Gan <ganze718@gmail.com>
2021-01-18 14:56:06 +0800

    Add gym_connector
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- ns-app/scratch/webrtc_test/gym_connector.cc
+++ ns-app/scratch/webrtc_test/gym_connector.cc
@@ -1,5 +1,7 @@
 #include "gym_connector.h"
 
+#include "json.hpp"
+
 using namespace webrtc;
 
 constexpr double kPacingFactor = 2.5f;
@@ -8,21 +10,31 @@ GymConnector::GymConnector() :
     current_bandwidth_(0) {
 }
 
+std::list<std::string> GymConnector::Step(BandwidthType bandwidth, uint32_t duration) {
+    SetBandwidth(bandwidth);
+    Wait(duration);
+    return ConsumeStates();
+}
+
+void GymConnector::Wait(std::uint32_t duration) const {
+
+}
+
 void GymConnector::SetBandwidth(BandwidthType bandwidth) {
     if (bandwidth == current_bandwidth_) {
         return;
     }
     {
-        std::unique_lock<std::shared_timed_mutex> guard(mutex_);
+        std::unique_lock<std::shared_timed_mutex> guard(mutex_bandiwidth_);
         current_bandwidth_ = bandwidth;
     }
 }
-#include <iostream>
+
 webrtc::NetworkControlUpdate GymConnector::GetNetworkControlUpdate(const webrtc::Timestamp& at_time) const {
     BandwidthType current_bandwidth = {0};
 
     {
-        std::shared_lock<std::shared_timed_mutex> guard(mutex_);
+        std::shared_lock<std::shared_timed_mutex> guard(mutex_bandiwidth_);
         current_bandwidth = current_bandwidth_;
     }
 
@@ -45,4 +57,36 @@ webrtc::NetworkControlUpdate GymConnector::GetNetworkControlUpdate(const webrtc:
     update.pacer_config->pad_window = webrtc::DataRate::BitsPerSec(0) * update.pacer_config->time_window;
 
     return update;
+}
+
+void GymConnector::ProduceStates(
+    int64_t arrival_time_ms,
+    size_t payload_size,
+    const webrtc::RTPHeader& header) {
+
+    nlohmann::json j;
+    j["arrival_time_ms"] = arrival_time_ms;
+    j["payload_size"] = payload_size;
+    j["send_time_ms"] = header.extension.absoluteSendTime;
+    j["transport_sequence_number"] = header.extension.transportSequenceNumber;
+    j["payload_type"] = header.payloadType;
+    j["sequence_number"] = header.sequenceNumber;
+    j["ssrc"] = header.ssrc;
+    j["padding_length"] = header.paddingLength;
+    j["header_length"] = header.headerLength;
+
+    const std::string stats = j.dump();
+    {
+        std::unique_lock<std::mutex> guard(mutex_stats_);
+        stats_.push_back(stats);
+    }
+}
+
+std::list<std::string> GymConnector::ConsumeStates() {
+    std::list<std::string> stats;
+    {
+        std::unique_lock<std::mutex> guard(mutex_stats_);
+        std::swap(stats, stats_);
+    }
+    return stats;
 }
