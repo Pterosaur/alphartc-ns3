fdaecdb4ce101443813c97d41f6099529c55f313
Ze Gan <ganze718@gmail.com>
2021-01-27 00:42:01 +0800

    Fix azurepipeline
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- mock_gym.py
+++ mock_gym.py
@@ -4,7 +4,7 @@
 import gym_connect
 
 if __name__ == "__main__":
-    conn = gym_connect.GymConnector("test_gym")
+    conn = gym_connect.GymConnector("gym")
     while True:
         stats = conn.step(1e6)
         if stats != None:
